35cb9eff64c64b2dfae68d2093b323ce1d49fec6
danmuji <135412839+kyj0924@users.noreply.github.com>
2023-06-29 19:27:02 +0900

    [Silver V] Title: 단어 정렬, Time: 4476 ms, Memory: 33472 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,15 @@
+N = int(input())
+word_list = []
+answerlist = []
+for i in range(0, N, 1):
+    word = input()
+    if word not in word_list:
+        word_list.append(word)
+word_list.sort()
+
+for j in range(1, 51, 1):
+    for i in range(0, len(word_list), 1):
+        if len(word_list[i]) == j:
+            answerlist.append(word_list[i])
+for i in range(0, len(answerlist), 1):
+    print(answerlist[i])
